660fbb9eab9d43f73dccd9438e14d63c6cfd0d3a
jmpman <131778000+jmpman1@users.noreply.github.com>
2025-11-06 11:30:33 -0300

    Change shebang to python3 and clean up comments
    Updated shebang and removed metadata comments.

--- dosenv.py
+++ dosenv.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env -S uv run --script
-# /// script
-# requires-python = ">=3.12"
-# dependencies = [
-#     "aiohttp>=3.9.0",
-#     "asyncio",
-# ]
-# ///
+#!/usr/bin/env python3
 """
 dosenv Tool
 =================
